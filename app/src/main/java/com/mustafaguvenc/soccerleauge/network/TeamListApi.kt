f5419c825ffa59cf17aafeb64d312c45c60846e0
mstfgvnc <mustafa_gvnc@windowslve.com>
2021-09-10 15:22:50 +0300

    Update

--- app/src/main/java/com/mustafaguvenc/soccerleauge/network/TeamListApi.kt
+++ app/src/main/java/com/mustafaguvenc/soccerleauge/network/TeamListApi.kt
@@ -7,7 +7,7 @@ import retrofit2.http.GET
 interface TeamListApi {
 
   //  https://gist.githubusercontent.com/mstfgvnc/a26b81f28f282044a4ed458dd57e114d/raw/676eb68023cc44593b73813a0634b6cbabb2916b/SoccerTeams
-    @GET("mstfgvnc/a26b81f28f282044a4ed458dd57e114d/raw/be47eb70d587db105e57c3182e209b0e376b866b/SoccerTeams")
+    @GET("mstfgvnc/a26b81f28f282044a4ed458dd57e114d/raw/64a8cea1cd75790878ab30f479ed9d9e9ce70595/SoccerTeams")
     fun getTeams(): Single<List<TeamModel>>
 
 }
